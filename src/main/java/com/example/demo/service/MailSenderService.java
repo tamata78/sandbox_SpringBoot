f3b36c73738bb306796879fabdb9cd9dff054039
tamata78 <5101956+tamata78@users.noreply.github.com>
2020-02-26 16:48:24 +0900

    modify sendmail source

--- src/main/java/com/example/demo/service/MailSenderService.java
+++ src/main/java/com/example/demo/service/MailSenderService.java
@@ -10,24 +10,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MailSenderService {
-    @Autowired
-    private MailSender sender;
-    
+	@Autowired
+	private MailSender sender;
+
 	@Autowired
 	private Environment env;
-    
+
 	static Logger logger = LoggerFactory.getLogger(MailSenderService.class);
 
-    public void sendMail(String subject, String textBody) {
-        SimpleMailMessage msg = new SimpleMailMessage();
+	public void sendMail(String subject, String textBody) {
+		SimpleMailMessage msg = new SimpleMailMessage();
 
-        msg.setFrom(env.getProperty("mail.xxx.from"));
-        msg.setTo(env.getProperty("mail.xxx.to"));
-        msg.setSubject(subject); //タイトルの設定
-        msg.setText(textBody); //本文の設定
+		msg.setFrom(env.getProperty("mail.xxx.from"));
+		msg.setTo(env.getProperty("mail.xxx.to"));
+		msg.setSubject(subject); // タイトルの設定
+		msg.setText(textBody); // 本文の設定
 
-        sender.send(msg);
-        
-        logger.info(subject + "：メール送信しました");
-    }
+		sender.send(msg);
+
+		logger.info(subject + "：メール送信しました");
+	}
+
 }
